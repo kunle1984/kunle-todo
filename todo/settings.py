3ba5de2ba26efd346951e5af49d85597dfbb5fed
kunle1984 <51406569+kunle1984@users.noreply.github.com>
2022-07-13 09:18:10 -0700

    update settings

--- todo/settings.py
+++ todo/settings.py
@@ -131,5 +131,3 @@ MEDIA_URL='/files/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import django_heroku
-django_heroku.settings(locals())
